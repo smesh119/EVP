e29ee389f496c1acd6b12ea10c048817d3b5de98
smesh119 <smesh119@gmail.com>
2014-10-27 16:10:27 -0400

    update

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
+// Click on "Stuck? Get a hint!" if you get stuck!
+
 $(document).ready(function(){
 
-	$("h2").click(function(e){
-		$(".section").slideToggle();
+	$(".purpose").click(function(e){
+		$(".section-purpose").slideToggle();
+	});
+  
+  $(".passion").click(function(e){
+		$(".section-passion").slideToggle();
+	});
+  
+  $(".community").click(function(e){
+		$(".section-community").slideToggle();
+	});
+  
+  $(".opportunity").click(function(e){
+		$(".section-opportunity").slideToggle();
 	});
 
 });
